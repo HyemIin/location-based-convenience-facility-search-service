db3cd4d6157548fc9ff390024b4e08f1c8f9acd8
Harry <114489245+HyemIin@users.noreply.github.com>
2023-09-15 22:30:29 +0900

    Add files via upload

--- src/main/java/org/example/domain/vo/CoordinateVo.java
+++ src/main/java/org/example/domain/vo/CoordinateVo.java
@@ -2,12 +2,15 @@ package org.example.domain.vo;
 
 import lombok.AllArgsConstructor;
 
-
+@AllArgsConstructor
 public class CoordinateVo {
+    private String x;
+    private String y;
 
-    public boolean isValidated(String point) {
-        double coordinatePoint = Double.parseDouble(point);
-        if (coordinatePoint < 0) {
+    public boolean isValidated(String x,String y) {
+        double xPoint = Double.parseDouble(x);
+        double yPoint = Double.parseDouble(y);
+        if (xPoint < 0 || yPoint < 0) {
             throw new IllegalArgumentException("좌표는 음수일 수 없습니다");
         } else {
             return true;
